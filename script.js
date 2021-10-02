99902f323d8f328c5ac2aee10ae02d188f5e0dcc
Tomás Bonasera <tomasbonasera9@gmail.com>
2021-10-02 11:08:51 -0300

    #

--- script.js
+++ script.js
@@ -4,17 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Sidenav.init(elems);
   });
 
-//Datepicker de fecha de inicio
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.datepicker_fecha_inicio');
-    var instances = M.Datepicker.init(elems);
-  });
-//Datepicker de fecha de finalizacion
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.datepicker_fecha_fin');
-    var instances = M.Datepicker.init(elems);
-  });
-
 //Sorteo rapido
 function aleatorio(minimo,maximo){
   return Math.round(Math.random()*(maximo-minimo)+parseInt(minimo));
@@ -26,4 +15,52 @@ function hacerSorteo(){
 
   var ganador = aleatorio(minimo,maximo);
   document.getElementById('numero_ganador').value = 'El número ganador es: '+ganador;
+}
+
+//Amigo invisible
+  //Datepicker de fecha de inicio
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker_fecha_inicio');
+    var instances = M.Datepicker.init(elems);
+  });
+  //Datepicker de fecha de finalizacion
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker_fecha_fin');
+    var instances = M.Datepicker.init(elems);
+  });
+//Validacion de campos
+let expresionRegularCelular = /^[0-9]{9}$/;
+
+function validacionDeCampos(){
+  var nombreSorteo = document.getElementById("nombre_sorteo").value;
+  var fechaInicio = document.getElementById("fecha_inicio").value;
+  var fechaFin = document.getElementById("fecha_fin").value;
+  var nombreParticipante = document.getElementById("nombre_participante").value;
+  var telParticipante = document.getElementById("cel_participante").value;
+
+  //El nombre del sorteo no puede estar vacio
+  if(!nombreSorteo.value){
+    nombreSorteo.focus();
+    return false;
+  }
+  
+  //Las fechas de inicio y fin no pueden estar vacias
+  if(!fechaInicio.value){
+    fechaInicio.focus();
+    return false;
+  }
+  if(!fechaFin.value){
+    fechaFin.focus();
+    return false;
+  }
+
+  //Los datos del participante no pueden estar vacios
+  if(!nombreParticipante.value){
+    nombreParticipante.focus();
+    return false;
+  }
+  if(!telParticipante.value){
+    telParticipante.focus();
+    return false;
+  }
 }
